4c344ddc73819334b8c52545da42a4dfb8161b43
Marlon Marques <marlon.belohd@gmail.com>
2022-02-18 20:36:47 -0300

    test(storage-adapter): clean through storageAdapter successfully

--- tests/infra/storage/storageAdapter.spec.ts
+++ tests/infra/storage/storageAdapter.spec.ts
@@ -1,8 +1,12 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { AddItemToStorage, GetItemToStorage } from '~/data/storage';
+import {
+  AddItemToStorage,
+  ClearStorage,
+  GetItemToStorage,
+} from '~/data/storage';
 
 describe('Infra: StorageAdapter', () => {
-  test('should add item with storageAdapter successfully', async () => {
+  test('should add item through storageAdapter successfully', async () => {
     const sut = new StorageAdapter();
     const storageKey = '@storage_key';
     const storageValue = 'storage_value';
@@ -10,19 +14,30 @@ describe('Infra: StorageAdapter', () => {
     expect(AsyncStorage.setItem).toBeCalledWith(storageKey, storageValue);
   });
 
-  test('should get item with storageAdapter successfully', async () => {
+  test('should get item through storageAdapter successfully', async () => {
     const sut = new StorageAdapter();
     const storageKey = '@storage_key';
     await sut.get(storageKey);
     expect(AsyncStorage.getItem).toBeCalledWith(storageKey);
   });
+
+  test('should clean through storageAdapter successfully', async () => {
+    const sut = new StorageAdapter();
+    await sut.clear();
+    expect(AsyncStorage.clear).toBeCalled();
+  });
 });
 
-class StorageAdapter implements AddItemToStorage, GetItemToStorage {
+class StorageAdapter
+  implements AddItemToStorage, GetItemToStorage, ClearStorage
+{
   async get(key: string): Promise<any> {
     await AsyncStorage.getItem(key);
   }
   async add(key: string, item: any): Promise<void> {
     await AsyncStorage.setItem(key, item);
+  }
+  async clear(): Promise<void> {
+    await AsyncStorage.clear();
   }
 }
