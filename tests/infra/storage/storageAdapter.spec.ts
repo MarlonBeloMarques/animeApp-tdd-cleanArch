302acf6e2fbfae1954855345ad875c395ce87917
Marlon Marques <marlon.belohd@gmail.com>
2022-02-18 20:30:32 -0300

    test(storage-adapter): get item with storageAdapter successfully

--- tests/infra/storage/storageAdapter.spec.ts
+++ tests/infra/storage/storageAdapter.spec.ts
@@ -1,5 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { AddItemToStorage } from '~/data/storage';
+import { AddItemToStorage, GetItemToStorage } from '~/data/storage';
 
 describe('Infra: StorageAdapter', () => {
   test('should add item with storageAdapter successfully', async () => {
@@ -9,9 +9,19 @@ describe('Infra: StorageAdapter', () => {
     await sut.add(storageKey, storageValue);
     expect(AsyncStorage.setItem).toBeCalledWith(storageKey, storageValue);
   });
+
+  test('should get item with storageAdapter successfully', async () => {
+    const sut = new StorageAdapter();
+    const storageKey = '@storage_key';
+    await sut.get(storageKey);
+    expect(AsyncStorage.getItem).toBeCalledWith(storageKey);
+  });
 });
 
-class StorageAdapter implements AddItemToStorage {
+class StorageAdapter implements AddItemToStorage, GetItemToStorage {
+  async get(key: string): Promise<any> {
+    await AsyncStorage.getItem(key);
+  }
   async add(key: string, item: any): Promise<void> {
     await AsyncStorage.setItem(key, item);
   }
