4387dfd6341fb523e1813199de4a538d3c17df48
Marlon Marques <marlon.belohd@gmail.com>
2022-02-18 13:11:32 -0300

    test(oauth-adapter): create makeSut

--- tests/infra/mocks/linkingMock.ts
+++ tests/infra/mocks/linkingMock.ts
@@ -1,6 +1,6 @@
 import { Linking } from 'react-native';
 
-export const mockLinking = () => {
+export const mockLinking = (): jest.Mocked<Linking> => {
   const mockedLinking = Linking as jest.Mocked<typeof Linking>;
   mockedLinking.openURL.mockResolvedValue('mockResolved');
   return mockedLinking;
--- tests/infra/oauth/oauthAdapter.spec.ts
+++ tests/infra/oauth/oauthAdapter.spec.ts
@@ -1,3 +1,4 @@
+import { Linking } from 'react-native';
 import { OAuthAdapter } from '~/infra/oauth';
 import { makeUrl } from '../../data/helpers/testFactories';
 import { mockLinking } from '../mocks/linkingMock';
@@ -6,9 +7,14 @@ jest.mock('react-native/Libraries/Linking/Linking');
 
 describe('Data: OAuthAdapter', () => {
   test('should request to openUrl once successfully', () => {
-    const mockedLinking = mockLinking();
-    const sut = new OAuthAdapter();
+    const [sut, mockedLinking] = makeSut();
     sut.redirect({ url: makeUrl() });
     expect(mockedLinking.openURL).toHaveBeenCalledTimes(1);
   });
 });
+
+const makeSut = (): [OAuthAdapter, jest.Mocked<Linking>] => {
+  const mockedLinking = mockLinking();
+  const sut = new OAuthAdapter();
+  return [sut, mockedLinking];
+};
