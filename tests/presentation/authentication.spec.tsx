586b9f9a74e3b219c544562e3bee8553ab6cf611
Marlon Marques <marlon.belohd@gmail.com>
2022-02-19 16:53:41 -0300

    test(authentication): should press button with success

--- tests/presentation/authentication.spec.tsx
+++ tests/presentation/authentication.spec.tsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 
 describe('Presentation: Authentication', () => {
   test('should show text button correct', () => {
-    const { getByTestId } = render(<Authentication />);
+    const { getByTestId } = render(
+      <Authentication onPressAuthentication={() => {}} />,
+    );
     const buttonId = 'authentication_id';
     const text = `LET'S BEGIN`;
     const button = getByTestId(buttonId);
     const textButton = button.findByType(Text).props.children;
     expect(textButton).toEqual(text);
   });
+
+  test('should press button with success', () => {
+    const onPressAuthenticationMock = jest.fn();
+    const { getByTestId } = render(
+      <Authentication onPressAuthentication={onPressAuthenticationMock} />,
+    );
+    const buttonId = 'authentication_id';
+    const button = getByTestId(buttonId);
+    fireEvent.press(button);
+    expect(onPressAuthenticationMock).toHaveBeenCalled();
+  });
 });
 
-const Authentication: React.FC = () => {
+type Props = {
+  onPressAuthentication: () => void;
+};
+
+const Authentication: React.FC<Props> = ({ onPressAuthentication }) => {
   return (
     <View>
-      <TouchableOpacity testID="authentication_id">
+      <TouchableOpacity
+        testID="authentication_id"
+        onPress={onPressAuthentication}
+      >
         <Text>{`LET'S BEGIN`}</Text>
       </TouchableOpacity>
     </View>
