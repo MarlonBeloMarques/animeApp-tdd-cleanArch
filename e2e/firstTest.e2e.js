099bb7810c55a39431ec362ec1d38b4f95bb1d90
Marlon Marques <marlon.belohd@gmail.com>
2022-02-28 19:39:56 -0300

    chore(detox): config tests for ios

---
+++ e2e/firstTest.e2e.js
@@ -0,0 +1,11 @@
+describe('_', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('_', async () => {});
+});
