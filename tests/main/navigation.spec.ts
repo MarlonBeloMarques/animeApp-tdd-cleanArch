0c9af26bcd34d0b38ae51ee1f55a1e9f5b16d2df
Marlon Marques <marlon.belohd@gmail.com>
2022-03-12 11:59:27 -0300

    test(main/navigation): call navigation actions to anime screen successfully

--- tests/main/navigation.spec.ts
+++ tests/main/navigation.spec.ts
@@ -2,6 +2,9 @@ import { CommonActions } from '@react-navigation/native';
 import { render, waitFor } from '@testing-library/react-native';
 import { NavigationActions, Routes } from '~/main/navigation';
 import Main from '~/main';
+import Animes from '~/presentation/screens/animes/animes';
+import { RemoteAnimeList } from '~/data/useCases';
+import { makeAnimeModel } from '../data/helpers';
 import { renderWithParams } from '../ui/helpers';
 
 describe('Main: Navigation', () => {
@@ -15,6 +18,27 @@ describe('Main: Navigation', () => {
       expect(navigateSpy).toHaveBeenCalledTimes(1);
     });
   });
+
+  test('should call navigation actions to anime screen successfully', async () => {
+    const navigateSpy = jest.spyOn(CommonActions, 'navigate');
+    jest
+      .spyOn(RemoteAnimeList.prototype, 'list')
+      .mockResolvedValueOnce(makeAnimeModel())
+      .mockClear();
+
+    const { getByType } = makeSut();
+
+    await waitFor(async () => {
+      NavigationActions.navigate(Routes.ANIMES);
+      expect(navigateSpy).toHaveBeenCalledWith({
+        name: Routes.ANIMES,
+        params: undefined,
+      });
+
+      const animesLength = getByType(Animes).props.animeList.length;
+      expect(animesLength).toBeTruthy();
+    });
+  });
 });
 
 const makeSut = () => {
@@ -23,11 +47,11 @@ const makeSut = () => {
     'setTopLevelNavigator',
   );
 
-  render(
+  const { UNSAFE_getByType } = render(
     renderWithParams({
       screen: Main,
     }),
   );
 
-  return { setTopLevelNavigatorSpy };
+  return { setTopLevelNavigatorSpy, getByType: UNSAFE_getByType };
 };
